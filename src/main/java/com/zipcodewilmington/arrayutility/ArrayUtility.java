8c3bd700df47006df275514febb3f8b19557a6a8
JeremyMccray <jeremy.mccray1337@gmail.com>
2020-03-11 19:36:52 -0400

    finished

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -4,6 +4,7 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Created by leon on 3/6/18.
@@ -15,31 +16,58 @@ public class ArrayUtility<T> {
         this.test=array;
     }
 
-    public T countDuplicatesInMerge(T[] arrayToMerge, T valueToEvaluate) {
-    return null;
-    }
-
-    public T getMostCommonFromMerge(T[] arrayToMerge) {
-        return null;
-    }
-
-    public T getNumberOfOccurrences(T valueToEvaluate) {
-        return null;
-    }
-
-    public T[] removeValue(T valueToRemove) {
-     @SuppressWarnings("unchecked")
-      T[] buffer = (T[]) Array.newInstance(valueToRemove.getClass(),test.length);
-     int counter = 0;
-        for (T element: buffer) {
-            if(!element.equals(valueToRemove)){
+    public Integer countDuplicatesInMerge(T[] arrayToMerge, T valueToEvaluate) {
+        int counter = 0;
+        for (T element:test) {
+            if(element.equals(valueToEvaluate)){
                 counter++;
             }
         }
-
-        for (int i = 0; i < buffer.length; i++) {
-
+        for (T elem: arrayToMerge) {
+            if(elem.equals(valueToEvaluate)){
+                counter++;
+            }
         }
-        return null;
+        return counter;
+    }
+
+    public T getMostCommonFromMerge(T[] arrayToMerge) {
+        ArrayList<T> merged = new ArrayList<>(Arrays.asList(arrayToMerge));
+        merged.addAll(Arrays.asList(test));
+        int counter = 0;
+        int mostCommon = 0;
+        for (int i = 0; i < merged.size(); i++) {
+            for (int j = 0; j < merged.size(); j++) {
+            if(merged.get(i) == merged.get(j)){
+                counter++;
+                if(counter > mostCommon){
+                    mostCommon = i;
+                }
+
+            }
+            }
+        }
+        return merged.get(mostCommon);
+    }
+
+    public Integer getNumberOfOccurrences(T valueToEvaluate) {
+       int counter = 0;
+        for (T element:test) {
+            if (element.equals(valueToEvaluate)){
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+    public T[] removeValue(T valueToRemove) {
+        ArrayList<T> tester = new ArrayList<>();
+        for (T element: test) {
+            if(!(element).equals(valueToRemove)){
+                tester.add(element);
+
+            }
+        }
+        return tester.toArray(Arrays.copyOf(this.test,tester.size()));
     }
 }
